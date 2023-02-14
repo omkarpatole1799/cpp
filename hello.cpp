ef6a26c27788bb3f6f3c5ecf68878a17a76b3a16
Omkar Patole <omkarpatole1799@gmail.com>
2023-02-14 02:39:43 -0800

    first

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout << "enter value of x: ";
+    cin >> x;
+    cout << "Enter value of y: ";
+    cin >> y;
+    cout << "Hello world!" << endl << "value of x is : " << x << endl << "value of y is " << y;
+}
