8bb1c200555cfc9f4ccbcf7f9a9e56dbf94110e5
Omkar Patole <omkarpatole1799@gmail.com>
2023-02-14 20:27:02 +0530

    cpp basics

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,110 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int x,y;
-    cout << "enter value of x: ";
-    cin >> x;
-    cout << "Enter value of y: ";
-    cin >> y;
-    cout << "Hello world!" << endl << "value of x is : " << x << endl << "value of y is " << y;
+int main()
+{
+    // int x,y;
+    // cout << "enter value of x: ";
+    // cin >> x;
+    // cout << "Enter value of y: ";
+    // cin >> y;
+    // cout << "Hello world!" << endl << "value of x is : " << x << endl << "value of y is " << y;
+
+    // getline(cin, varName)  this function takes all the string in one line
+
+    // string myStr;
+    // getline(cin, myStr); // this takes full line
+    // cin >> myStr;  // this only takes first word in line
+    // cout << myStr;
+    // =======================================================================================
+    // int age;
+    // cout << "Enter your age";
+    // cin >> age;
+    // if (age >= 18)
+    // {
+    //     cout << "you are adult";
+    // }
+    // else
+    // {
+    //     cout << "you are child";
+    // }
+    // return 0;
+    // =======================================================================================
+
+    // int age;
+    // cin >> age;
+    // if (age < 18)
+    // {
+    //     cout << "Not eligible for job";
+    // }
+    // else if (age >= 18)
+    // {
+    //     cout << "eligile for job";
+    // }
+    // else if (age >= 55 && age <= 57)
+    // {
+    //     cout << "retiring soon";
+    // }
+    // else if (age > 57)
+    // {
+    //     cout << "retiring!!";
+    // }
+
+    // int age;
+    // cin >> age;
+    // if (age < 18)
+    // {
+    //     cout << "Not eligible for job";
+    // }
+    // else if (age <= 57)
+    // {
+    //     cout << "eligile for job";
+    //     if (age >= 55)
+    //     {
+    //         cout << ",but retiring soon";
+    //     }
+    // }
+    // else if (age > 57)
+    // {
+    //     cout << "retiring!!";
+    // }
+    // =======================================================================================
+    // 1d arr
+    // double myArr[5] = {1, 2, 3, 4.3, 5};
+    // cout << myArr[3];
+
+    // 2d arr int arr[row][column]
+    // int arr[3][5];
+    // arr[1][3] = 555;
+    // cout << arr[1][3];
+
+    // =======================================================================================
+    // string
+    // string myStr = "omkar";
+    // cout << myStr[3]; // this print the value at index 3
+
+    // cout << myStr[myStr.size()-1];
+    // =======================================================================================
+    // for loops and while loops
+
+    // for (int i = 1; i <=5; i++){
+    //     cout << "hello world \n";
+    // }
+
+    // int i = 1;
+    // while (i <= 5)
+    // {
+    //     cout << "hello world \n";
+    //     i++;
+    // }
+
+    // int i = 6;
+    // do
+    // {
+    //     cout << "hello world \n";
+    // } while (i <= 5);
+
+    // =======================================================================================
+    // =======================================================================================
+    // =======================================================================================
+    return 0;
 }
